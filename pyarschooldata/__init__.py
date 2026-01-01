778e864d3d4d46ededd54ea69db5753f7275eff0
almartin82 <almartin@gmail.com>
2025-12-31 20:11:17 -0500

    Add Python wrapper package

---
+++ pyarschooldata/__init__.py
@@ -0,0 +1,16 @@
+"""
+pyarschooldata - Python wrapper for Arkansas school enrollment data.
+
+Thin rpy2 wrapper around the arschooldata R package.
+Returns pandas DataFrames.
+"""
+
+from .core import (
+    fetch_enr,
+    fetch_enr_multi,
+    tidy_enr,
+    get_available_years,
+)
+
+__version__ = "0.1.0"
+__all__ = ["fetch_enr", "fetch_enr_multi", "tidy_enr", "get_available_years"]
